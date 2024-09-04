b39b6287b280d3bfcdce38bbb03e9b14380b3985
Lohit <lohitmkudlannavar42@gmail.com>
2024-09-04 17:47:30 +0530

    Swagger 3.0 Integration

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const authRoutes = require("./src/routes/authRoutes");
 const groupRoutes = require("./src/routes/groupRoutes");
 const expenseRoutes = require("./src/routes/expenseRoutes");
@@ -11,6 +13,9 @@ const PORT = process.env.PORT;
 
 // Middleware
 app.use(bodyParser.json());
+
+// Swagger Route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes
 app.use('/api/auth', authRoutes);
